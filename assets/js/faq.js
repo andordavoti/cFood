ceb3610b9524ac6bf95f7c05862f73e43c9f4471
Andor Davoti <andor.davoti@gmail.com>
2020-11-14 12:27:14 +0100

    fixed active class not toggling correctly when auto closing open accordions

--- assets/js/faq.js
+++ assets/js/faq.js
@@ -5,17 +5,22 @@ const closeAllPanels = () => {
   panel.forEach((element) => {
     element.style.display = "none";
   });
+
+  accordion.forEach((element) => {
+    element.classList.remove("active");
+  });
 };
 
 for (let i = 0; i < accordion.length; i++) {
   accordion[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-
     const currentPanel = this.nextElementSibling;
+
     if (currentPanel.style.display === "block") {
       currentPanel.style.display = "none";
+      this.classList.remove("active");
     } else {
       closeAllPanels();
+      this.classList.add("active");
       currentPanel.style.display = "block";
     }
   });
