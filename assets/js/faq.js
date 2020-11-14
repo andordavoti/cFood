7cb8e57e6628e1817d0f85fb5d8d02f6184c4239
Andor Davoti <andor.davoti@gmail.com>
2020-11-14 12:16:10 +0100

    Revert "more CSS cleanup"
    This reverts commit a004ec707a2739bca1b2fcea80df7b68c0d256d4.

--- assets/js/faq.js
+++ assets/js/faq.js
@@ -1,11 +1,9 @@
 const accordion = document.querySelectorAll(".accordion");
 const panel = document.querySelectorAll(".panel");
 
-const closeAllPanels = () => {
-  panel.forEach((element) => {
-    element.style.display = "none";
-  });
-};
+panel.forEach((element) => {
+  element.style.display = "none";
+});
 
 for (let i = 0; i < accordion.length; i++) {
   accordion[i].addEventListener("click", function () {
@@ -15,10 +13,7 @@ for (let i = 0; i < accordion.length; i++) {
     if (currentPanel.style.display === "block") {
       currentPanel.style.display = "none";
     } else {
-      closeAllPanels();
       currentPanel.style.display = "block";
     }
   });
 }
-
-window.onload = () => closeAllPanels();
