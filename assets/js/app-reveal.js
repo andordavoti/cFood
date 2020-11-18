a957a421df2824761d18097232d3fc1bd895389e
Andor Davoti <andor.davoti@gmail.com>
2020-11-18 12:40:13 +0100

    changed timeout

--- assets/js/app-reveal.js
+++ assets/js/app-reveal.js
@@ -3,4 +3,4 @@ const appBgEl = document.querySelector(".fs-bg-img");
 setTimeout(() => {
   appBgEl.style.backgroundImage = "url(../assets/img/bg/app-bg-2.jpg)";
   appBgEl.style.filter = "brightness(80%)";
-}, 2000);
+}, 1500);
