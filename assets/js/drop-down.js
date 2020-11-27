a4ac24f88ee56f8f82bae70558109cfe0f5260f1
Andor Davoti <andor.davoti@gmail.com>
2020-11-27 13:59:48 +0100

    added comments to JS code

--- assets/js/drop-down.js
+++ assets/js/drop-down.js
@@ -1,27 +1,35 @@
-const accordions = document.querySelectorAll(".accordion");
-const panel = document.querySelectorAll(".panel");
+// Get all the accordion elements from the DOM as an array
+const accordionsEl = document.querySelectorAll(".accordion");
+// Get all the panel elements from the DOM as an array
+const panelEl = document.querySelectorAll(".panel");
 
+// Loop through all the panel and accourdion elements and put them in the closed state
 const closeAllPanels = () => {
-  panel.forEach((element) => {
-    element.classList.remove("panel-open");
+  panelEl.forEach((el) => {
+    el.classList.remove("panel-open");
   });
 
-  accordions.forEach((element) => {
-    element.classList.remove("active");
+  accordionsEl.forEach((el) => {
+    el.classList.remove("active");
   });
 };
 
-for (const accordion of accordions) {
-  accordion.addEventListener("click", function () {
+// Loop through all accordion elements and add an event listener function
+accordionsEl.forEach((el) => {
+  el.addEventListener("click", function () {
+    // Get the panel element that belongs to the accordion element
     const currentPanel = this.nextElementSibling;
 
+    // If the panel is open, then close it
     if (currentPanel.className.includes("panel-open")) {
       currentPanel.classList.remove("panel-open");
       this.classList.remove("active");
-    } else {
+    }
+    // If the panel is closed then open the one that's presses and close the other ones
+    else {
       closeAllPanels();
       this.classList.add("active");
       currentPanel.classList.add("panel-open");
     }
   });
-}
+});
--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,17 +1,25 @@
-const hamburger = document.querySelector(".hamburger");
+// Get the hamburger element from the DOM
+const hamburgerEl = document.querySelector(".hamburger");
 
-const navLinks = document.querySelector("nav .links");
-const links = document.querySelectorAll("nav .links .link");
+// Get the nav links element from the DOM
+const navLinksEl = document.querySelector("nav .links");
+// Get all the nav link elements from the DOM as an array
+const linksEl = document.querySelectorAll("nav .links .link");
 
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-  links.forEach((link) => {
-    link.classList.toggle("fade");
+// Add an event listener function that runs every time the user clicks on the hamburger element
+hamburgerEl.addEventListener("click", () => {
+  // Toggle the open class for the nav links element
+  navLinksEl.classList.toggle("open");
+
+  // Loop through the array of link elements and toggle the fade class
+  linksEl.forEach((linkEl) => {
+    linkEl.classList.toggle("fade");
   });
 
-  if (navLinks.classList.contains("open")) {
-    hamburger.src = "assets/img/icons/close.png";
+  // change the image of the hamburger menu
+  if (navLinksEl.classList.contains("open")) {
+    hamburgerEl.src = "assets/img/icons/close.png";
   } else {
-    hamburger.src = "assets/img/icons/menu.png";
+    hamburgerEl.src = "assets/img/icons/menu.png";
   }
 });
