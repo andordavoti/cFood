39d93612c331d4ed3b4676b901cce55417cf8c07
Andor Davoti <andor.davoti@gmail.com>
2020-11-27 12:07:57 +0100

    fixed app-reveal animation to not use JS

--- assets/js/app-reveal.js
+++
@@ -1,6 +0,0 @@
-const appBgEl = document.querySelector(".fs-bg-img");
-
-setTimeout(() => {
-  appBgEl.style.backgroundImage = "url(assets/img/bg/app-bg-2.jpg)";
-  appBgEl.style.filter = "brightness(80%)";
-}, 1500);
